89ce7d64c5fb119d3ff40f81aa947df14ead34db
Adán  López Alatorre <adanlopezalatorre@Adans-MacBook-Air.local>
2019-11-24 14:25:06 -0600

    Usamos glVertex3 en lugar del otro

--- src/OpenGL/main.cpp
+++ src/OpenGL/main.cpp
@@ -1,198 +1,50 @@
-/*
- * OGL01Shape3D.cpp: 3D Shapes
- */
-#include <iostream>
+#include <GL/glew.h>
 #include <fstream>
+#include <iostream>
 #include <sstream>
-#include <GLUT/glut.h>  // GLUT, include glu.h and gl.h
+#include <cmath>
+#include <iomanip>
+#include <GLFW/glfw3.h>
+#include "matrix.h"
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
 #define POINT_COUNT 24
 #define MAX_PRISM 50
 
 using namespace std;
 
-/* Global variables */
-char title[] = "3D Shapes";
 GLfloat marioPoints[MAX_PRISM][POINT_COUNT * 3];
 int prismCount = 0;
 
-/*  Create checkerboard texture  */
-#define checkImageWidth 65
-#define checkImageHeight 65
-static GLubyte checkImage[checkImageHeight][checkImageWidth][4];
-int width,height,colours;
+void init(void);
+/**/
+void keyCallback( GLFWwindow *window, int key, int scancode, int action, int mods );
+/**/
+void drawCube2(GLfloat vertices[POINT_COUNT * 3], GLubyte *color);
+/**/
+void drawCube3(GLfloat vertices[POINT_COUNT * 3]);
 
-void makeCheckImage(void)
-{
-    int i, j, c;
-    char keyword[2];
-    string comment;
-    //Generating Checker Pattern
-    /*for (i = 0; i < checkImageHeight; i++) {
-     for (j = 0; j < checkImageWidth; j++) {
-     c = ((((i&0x8)==0)^((j&0x8))==0))*255;
-     checkImage[i][j][0] = (GLubyte) c;
-     checkImage[i][j][1] = (GLubyte) c;
-     checkImage[i][j][2] = (GLubyte) c;
-     checkImage[i][j][3] = (GLubyte) 255;
-     }
-     }*/
-    
-    //Reading texture file
-    ifstream inFile;
-    
-    inFile.open("/Users/adanlopezalatorre/Desktop/Raul.ppm");
-    if (!inFile) {
-        cout << "Unable to open file";
-        exit(1); // terminate with error
+GLfloat rotationX = 0.0f;
+GLfloat rotationY = 0.0f;
+
+GLfloat traslationX = 0.0f;
+GLfloat traslationY = 0.0f;
+GLfloat traslationZ = 0.0f;
+
+GLfloat scaleX = 1.0f;
+GLfloat scaleY = 1.0f;
+GLfloat scaleZ = 1.0f;
+
+void setAllPoints() {
+    for(int i = 0; i < prismCount; i++) {
+        drawCube2(marioPoints[i], colors[i]);
+//        drawCube3(marioPoints[i]);
     }
-    
-    inFile >> keyword;
-    cout << keyword << " ";
-    inFile >> comment;
-    cout << comment << " ";
-    inFile >> comment;
-    cout << comment << " ";
-    inFile >> comment;
-    cout << comment << " ";
-    inFile >> comment;
-    cout << comment << " ";
-    
-    inFile >> width;
-    cout << width;
-    inFile >> height;
-    cout << height << "\n";
-    inFile >> colours;
-    cout << colours;
-    cout << endl;
-    for (i = 0; i < height; i++) {
-        for (j = 0; j < width; j++) {
-            inFile >> c;
-            checkImage[height-i][j][0] = (GLubyte) c;
-            //cout << c << " ";
-            inFile >> c;
-            checkImage[height-i][j][1] = (GLubyte) c;
-            //cout << c << " ";
-            inFile >> c;
-            checkImage[height-i][j][2] = (GLubyte) c;
-            //cout << c << " ";
-            checkImage[height-i][j][3] = (GLubyte) 255;
-            //cout << "255n";
-        }
-        //cout << endl;
-    }
-    inFile.close();
 }
 
-/* Initialize OpenGL Graphics */
-void initGL() {
-   glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
-   glClearDepth(1.0f);                   // Set background depth to farthest
-   glEnable(GL_DEPTH_TEST);   // Enable depth testing for z-culling
-   glDepthFunc(GL_LEQUAL);    // Set the type of depth-test
-   glShadeModel(GL_SMOOTH);   // Enable smooth shading
-   glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
-   makeCheckImage();
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_NEAREST);
-    /*glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, checkImageWidth,
-                 checkImageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE,
-                 checkImage);
-    gluBuild2DMipmaps(GL_TEXTURE_2D,1,checkImageWidth,checkImageHeight,GL_RGBA,GL_UNSIGNED_BYTE,checkImage);*/
-}
-
-/* Handler for window-repaint event. Called back when the window first appears and
-   whenever the window needs to be re-painted. */
-void display() {
-   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
-   glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
- 
-   // Render a color-cube consisting of 6 quads with different colors
-   glLoadIdentity();                 // Reset the model-view matrix
-   glTranslatef(1.5f, 0.0f, -8.0f);  // Move right and into the screen
-
-glEnable(GL_TEXTURE_2D);
-   glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
-      // Top face (y = 1.0f)
-      // Define vertices in counter-clockwise (CCW) order with normal pointing out
-
-	for(int u = 0; u < prismCount; u++)
-	for(int i = 0; i < POINT_COUNT; i += 4) {
-      //glColor3f(0.0f, 1.0f, 0.0f);     // Green
-
-	
-	for(int j = i; j < i + 4; j++) {
-		auto entr = 200.0f;
-		glTexCoord2f(4.0, 0.0);
-		glVertex3f( marioPoints[u][j * 3]/entr, marioPoints[u][j * 3 + 1]/entr, marioPoints[u][j * 3 + 2]/entr);
-		//for(int r = 0; r < 3; r++) cout << marioPoints[u][j * 3 + r] << ' '; cout << '\n';
-	}
-	}
-
-      /*glColor3f(0.0f, 1.0f, 0.0f);     // Green
-
-      glVertex3f( 1.0f, 1.0f, -1.0f);
-      glVertex3f(-1.0f, 1.0f, -1.0f);
-      glVertex3f(-1.0f, 1.0f,  1.0f);
-      glVertex3f( 1.0f, 1.0f,  1.0f);
-
-      // Bottom face (y = -1.0f)
-      glColor3f(1.0f, 0.5f, 0.0f);     // Orange
-      glVertex3f( 1.0f, -1.0f,  1.0f);
-      glVertex3f(-1.0f, -1.0f,  1.0f);
-      glVertex3f(-1.0f, -1.0f, -1.0f);
-      glVertex3f( 1.0f, -1.0f, -1.0f);
-
-      // Front face  (z = 1.0f)
-      glColor3f(1.0f, 0.0f, 0.0f);     // Red
-      glVertex3f( 1.0f,  1.0f, 1.0f);
-      glVertex3f(-1.0f,  1.0f, 1.0f);
-      glVertex3f(-1.0f, -1.0f, 1.0f);
-      glVertex3f( 1.0f, -1.0f, 1.0f);
-
-      // Back face (z = -1.0f)
-      glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
-      glVertex3f( 1.0f, -1.0f, -1.0f);
-      glVertex3f(-1.0f, -1.0f, -1.0f);
-      glVertex3f(-1.0f,  1.0f, -1.0f);
-      glVertex3f( 1.0f,  1.0f, -1.0f);
-
-      // Left face (x = -1.0f)
-      glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-      glVertex3f(-1.0f,  1.0f,  1.0f);
-      glVertex3f(-1.0f,  1.0f, -1.0f);
-      glVertex3f(-1.0f, -1.0f, -1.0f);
-      glVertex3f(-1.0f, -1.0f,  1.0f);
-
-      // Right face (x = 1.0f)
-      glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
-      glVertex3f(1.0f,  1.0f, -1.0f);
-      glVertex3f(1.0f,  1.0f,  1.0f);
-      glVertex3f(1.0f, -1.0f,  1.0f);
-      glVertex3f(1.0f, -1.0f, -1.0f);*/
-   glEnd();  // End of drawing color-cube
-glDisable(GL_TEXTURE_2D);
-
-   glutSwapBuffers();  // Swap the front and back frame buffers (double buffering)
-}
-
-/* Handler for window re-size event. Called back when the window first appears and
-   whenever the window is re-sized with its new width and height */
-void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integer
-   // Compute aspect ratio of the new window
-   if (height == 0) height = 1;                // To prevent divide by 0
-   GLfloat aspect = (GLfloat)width / (GLfloat)height;
- 
-   // Set the viewport to cover the new window
-   glViewport(0, 0, width, height);
- 
-   // Set the aspect ratio of the clipping volume to match the viewport
-   glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
-   glLoadIdentity();             // Reset
-   // Enable perspective projection with fovy, aspect, zNear and zFar
-   gluPerspective(45.0f, aspect, 0.1f, 100.0f);
-}
-
-/* Main function: GLUT runs as a console application starting at main() */
-int main(int argc, char** argv) {
+int main() {
+    GLFWwindow *window;
     std::string inp;
     std::ifstream in("points.txt");
     for(prismCount = 0; std::getline(in, inp); prismCount++) {
@@ -205,14 +57,206 @@ int main(int argc, char** argv) {
         }
     }
     in.close();
-   glutInit(&argc, argv);            // Initialize GLUT
-   glutInitDisplayMode(GLUT_DOUBLE); // Enable double buffered mode
-   glutInitWindowSize(640, 480);   // Set the window's initial width & height
-   glutInitWindowPosition(0, 0); // Position the window's initial top-left corner
-   glutCreateWindow(title);          // Create window with the given title
-   glutDisplayFunc(display);       // Register callback handler for window re-paint event
-   glutReshapeFunc(reshape);       // Register callback handler for window re-size event
-   initGL();                       // Our own OpenGL initialization
-   glutMainLoop();                 // Enter the infinite event-processing loop
-   return 0;
+    
+    
+    // Initialize the library
+    if (!glfwInit()) {
+        return -1;
+    }
+    
+    // Create a window mode window and its OpenGL context
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Mario model", NULL, NULL);
+    
+    /**/
+    glfwSetKeyCallback( window, keyCallback );
+    glfwSetInputMode( window, GLFW_STICKY_KEYS, 1 );
+    
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize( window, &screenWidth, &screenHeight );
+    /**/
+    
+    if(!window){
+        glfwTerminate();
+        return -1;
+    }
+    
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+    
+    glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1000);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    
+    /**/
+    GLfloat halfScreenWidth = SCREEN_WIDTH / 2;
+    GLfloat halfScreenHeight = SCREEN_HEIGHT / 2;
+    /**/
+    
+    // Loop until window closes
+    while(!glfwWindowShouldClose(window)) {
+
+        glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
+        
+        glEnable(GL_DEPTH_TEST);
+        
+        /**/
+        glPushMatrix( );
+        glTranslatef( halfScreenWidth, halfScreenHeight, -500 );
+        glScalef(scaleX, scaleY, scaleZ);
+        scaleX = scaleY = scaleZ = 1;
+        glRotatef( rotationX, 1, 0, 0 );
+        glRotatef( rotationY, 0, 1, 0 );
+        glTranslatef( -halfScreenWidth + traslationX, -halfScreenHeight + traslationY, 500  + traslationZ);
+        /**/
+        
+        setAllPoints();
+//        GLfloat vert[] = {563.000, 195.000, -470.000, 613.000, 195.000, -470.000, 613.000, 165.000, -470.000, 563.000, 165.000, -470.000, 563.000, 195.000, -570.000, 613.000, 195.000, -570.000, 613.000, 165.000, -570.000, 563.000, 165.000, -570.000, 563.000, 195.000, -470.000, 563.000, 195.000, -570.000, 563.000, 165.000, -570.000, 563.000, 165.000, -470.000, 613.000, 195.000, -470.000, 613.000, 195.000, -570.000, 613.000, 165.000, -570.000, 613.000, 165.000, -470.000, 563.000, 195.000, -470.000, 563.000, 195.000, -570.000, 613.000, 195.000, -570.000, 613.000, 195.000, -470.000, 563.000, 195.000, -570.000, 563.000, 195.000, -570.000, 613.000, 195.000, -570.000, 613.000, 195.000, -570.000};
+//        drawCube3(vert);
+        
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+        
+        // Poll for and process events
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    return 0;
 }
+
+void drawCube2(GLfloat vertices[POINT_COUNT * 3], GLubyte *color){
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+    glColorPointer( 4, GL_UNSIGNED_BYTE, 0, color );
+    glVertexPointer(3, GL_FLOAT, 0, vertices);
+    glDrawArrays(GL_QUADS, 0, 24);
+    glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_COLOR_ARRAY);
+}
+
+void drawCube3(GLfloat vertices[POINT_COUNT * 3]) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
+    glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
+    glLoadIdentity();                 // Reset the model-view matrix
+
+    
+    glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
+         int i = 0;
+         // Top face (y = 1.0f)
+         // Define vertices in counter-clockwise (CCW) order with normal pointing out
+         glColor3f(0.0f, 1.0f, 0.0f);     // Green
+         glVertex3f(vertices[i + 0], vertices[i + 1], vertices[i + 2]);
+         glVertex3f(vertices[i + 3], vertices[i + 4], vertices[i + 5]);
+         glVertex3f(vertices[i + 6], vertices[i + 7], vertices[i + 8]);
+         glVertex3f(vertices[i + 9], vertices[i + 10], vertices[i + 11]);
+    
+         i+=12;
+         // Bottom face (y = -1.0f)
+         glColor3f(1.0f, 0.5f, 0.0f);     // Orange
+         glVertex3f(vertices[i + 0], vertices[i + 1], vertices[i + 2]);
+         glVertex3f(vertices[i + 3], vertices[i + 4], vertices[i + 5]);
+         glVertex3f(vertices[i + 6], vertices[i + 7], vertices[i + 8]);
+         glVertex3f(vertices[i + 9], vertices[i + 10], vertices[i + 11]);
+    
+         i+=12;
+         // Front face  (z = 1.0f)
+         glColor3f(1.0f, 0.0f, 0.0f);     // Red
+         glVertex3f(vertices[i + 0], vertices[i + 1], vertices[i + 2]);
+         glVertex3f(vertices[i + 3], vertices[i + 4], vertices[i + 5]);
+         glVertex3f(vertices[i + 6], vertices[i + 7], vertices[i + 8]);
+         glVertex3f(vertices[i + 9], vertices[i + 10], vertices[i + 11]);
+    
+         i+=12;
+         // Back face (z = -1.0f)
+         glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
+         glVertex3f(vertices[i + 0], vertices[i + 1], vertices[i + 2]);
+         glVertex3f(vertices[i + 3], vertices[i + 4], vertices[i + 5]);
+         glVertex3f(vertices[i + 6], vertices[i + 7], vertices[i + 8]);
+         glVertex3f(vertices[i + 9], vertices[i + 10], vertices[i + 11]);
+    
+         i+=12;
+         // Left face (x = -1.0f)
+         glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+         glVertex3f(vertices[i + 0], vertices[i + 1], vertices[i + 2]);
+         glVertex3f(vertices[i + 3], vertices[i + 4], vertices[i + 5]);
+         glVertex3f(vertices[i + 6], vertices[i + 7], vertices[i + 8]);
+         glVertex3f(vertices[i + 9], vertices[i + 10], vertices[i + 11]);
+    
+         i+=12;
+         // Right face (x = 1.0f)
+         glColor3f(1.0f, 1.0f, 1.0f);     // Magenta
+         glVertex3f(vertices[i + 0], vertices[i + 1], vertices[i + 2]);
+         glVertex3f(vertices[i + 3], vertices[i + 4], vertices[i + 5]);
+         glVertex3f(vertices[i + 6], vertices[i + 7], vertices[i + 8]);
+         glVertex3f(vertices[i + 9], vertices[i + 10], vertices[i + 11]);
+      glEnd();  // End of drawing color-cube
+}
+
+void keyCallback( GLFWwindow *window, int key, int scancode, int action, int mods )
+{
+    // actions are GLFW_PRESS, GLFW_RELEASE or GLFW_REPEAT
+    if ( action == GLFW_RELEASE || action == GLFW_PRESS )
+    {
+        const GLfloat scaleSpeed = 1.1;
+        const GLfloat rotationSpeed = (action == GLFW_PRESS)?0.1:(-0.1);
+        const GLfloat translationSpeed = (action == GLFW_PRESS)? 2: -2;
+        switch ( key )
+        {
+            case GLFW_KEY_UP:
+                rotationX -= rotationSpeed;
+                break;
+            case GLFW_KEY_DOWN:
+                rotationX += rotationSpeed;
+                break;
+            case GLFW_KEY_RIGHT:
+                rotationY += rotationSpeed;
+                break;
+            case GLFW_KEY_LEFT:
+                rotationY -= rotationSpeed;
+                break;
+                
+            case GLFW_KEY_A:
+                traslationX -= translationSpeed;
+                break;
+            case GLFW_KEY_B:
+                traslationX += translationSpeed;
+                break;
+            case GLFW_KEY_C:
+                traslationY += translationSpeed;
+                break;
+            case GLFW_KEY_D:
+                traslationY -= translationSpeed;
+                break;
+            case GLFW_KEY_E:
+                traslationZ += translationSpeed;
+                break;
+            case GLFW_KEY_F:
+                traslationZ -= translationSpeed;
+                break;
+                
+            case GLFW_KEY_0:
+                scaleX = 1/scaleSpeed;
+                break;
+            case GLFW_KEY_1:
+                scaleX = scaleSpeed;
+                break;
+            case GLFW_KEY_2:
+                scaleY = 1/scaleSpeed;
+                break;
+            case GLFW_KEY_3:
+                scaleY = scaleSpeed;
+                break;
+            case GLFW_KEY_4:
+                scaleZ = 1/scaleSpeed;
+                break;
+            case GLFW_KEY_5:
+                scaleZ = scaleSpeed;
+                break;
+        }
+        
+        
+    }
+}
+
